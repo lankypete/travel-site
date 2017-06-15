44c4539f942afcb16bb7a2e1d0b2ddb5a223bdbb
Dean Pete <lankypete7@gmail.com>
2017-06-14 18:44:54 -0600

    Completed Large Hero Block And some Styles

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,21 @@
+//styles.js
+var gulp = require("gulp"),
+postcss = require("gulp-postcss"),
+autoprefixer = require("autoprefixer"),
+cssvars = require("postcss-simple-vars"),
+nested =require("postcss-nested"),
+cssImport = require("postcss-import"),
+mixins = require("postcss-mixins");
+
+//the styles task pipes all css modules, and base files, and all into postcss, 
+//and then to the final stylesheet inside the temp folder
+//(called from gulp cssInject dependancies)
+gulp.task('styles', function() {
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssImport, mixins, nested, cssvars, autoprefixer]))
+		.on('error', function(errorInfo){ //so that gulp tasks don't quit on accidental css error
+			console.log(errorInfo.toString());
+			this.emit('end');
+		})
+		.pipe(gulp.dest('./app/temp/styles'));
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,28 @@
+var gulp = require("gulp"),
+watch = require("gulp-watch"),
+browserSync = require("browser-sync").create();
+
+gulp.task('watch', function(){
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir : "app" //the base folder
+		}
+	});
+
+	//syntax watch('file to be watched', what it will do)
+	//this watches the index file for saves and then refreshes browser
+	watch('./app/index.html', function (){
+		browserSync.reload();
+	});
+	//watches css files for reload and calls the gulp styles task above
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start('cssInject');
+	});
+});
+
+
+gulp.task('cssInject', ['styles']/*<--dependencies*/, function() {
+	return gulp.src('./app/temp/styles/styles.css')
+	.pipe(browserSync.stream()); //injects the css directly into browser (without refresh)
+});
