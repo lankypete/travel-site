50a3a69df9347eb0bd06c5e9fed5a4521f0b93e2
Dean Pete <lankypete7@gmail.com>
2017-06-20 04:52:07 -0600

    Complete Auto Sprite

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp'),
+svgSprite = require('gulp-svg-sprite'),
+rename = require('gulp-rename'),
+del = require('del');
+
+var config = {
+	mode : {
+		css : {
+			sprite : 'sprite.svg',
+			render : {
+				css : {
+					template: './gulp/templates/sprite.css'
+				}
+			}
+		}
+	}
+}
+
+//the first task will delete the latest sprite folders 
+gulp.task('beginClean', function() {
+	return del(['./app/temp/sprite', './app/assets/images/sprites']);
+});
+
+gulp.task('createSprite', ['beginClean'], function() {
+	return gulp.src('./app/assets/images/icons/**/*.svg')
+		.pipe(svgSprite(config))
+		.pipe(gulp.dest('./app/temp/sprite/'));
+});
+
+gulp.task('copySpriteGraphic', ['createSprite'], function(){
+	return gulp.src('./app/temp/sprite/css/**/*.svg')
+		.pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+//seperate tesk to move the generated css file into our tidy css/modules folder
+//with a name _sprite.css
+gulp.task('copySpriteCSS', ['createSprite'], function() {
+	return gulp.src('./app/temp/sprite/css/*.css')
+		.pipe(rename('_sprite.css'))
+		.pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+//since we don't really need the app/temp/sprite folder anymore
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function() {
+	return del('./app/temp/sprite');
+});
+
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
